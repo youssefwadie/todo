9c71613a5209aa477e2a5292aac3bc2194b9ef00
Youssef Wadie <youssefwadie14@gmail.com>
2022-08-05 14:02:25 +0200

    find tests

--- src/test/java/com/github/youssefwadie/todo/services/UserServiceTests.java
+++ src/test/java/com/github/youssefwadie/todo/services/UserServiceTests.java
@@ -1,6 +1,9 @@
 package com.github.youssefwadie.todo.services;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.github.youssefwadie.todo.dao.todo.TodoDao;
 import com.github.youssefwadie.todo.dao.user.UserDao;
+import com.github.youssefwadie.todo.exceptions.UserNotFoundException;
 import com.github.youssefwadie.todo.model.User;
 
 
@@ -49,6 +53,7 @@ public class UserServiceTests {
         Mockito.doAnswer(invocation -> {
         	User passedUser = invocation.getArgument(0);
         	System.out.printf("Deleting user ... %d - with email %s%n", passedUser.getId(), passedUser.getEmail());
+        	assertThat(passedUser.getId()).isEqualTo(userId);
         	return null;
         }).when(userDao).delete(user);
         
@@ -58,6 +63,26 @@ public class UserServiceTests {
     }
 
     @Test
-    void testFindUserById() {
+    void testFindUserByIdThrowsException() throws UserNotFoundException {
+    	Long userId = 84568L;
+    	Optional<User> userOptional = Optional.empty();
+    	Mockito.when(userDao.findById(userId)).thenReturn(userOptional);
+    	
+    	assertThrows(UserNotFoundException.class, ()-> {
+    		userService.findById(userId);
+    	});
+    
+    }
+    
+    
+    @Test
+    void testFindUserById() throws UserNotFoundException {
+    	Long userId = 1L;
+    	User user = new User(userId, "", "");
+    	Optional<User> userOptional = Optional.of(user);
+    	Mockito.when(userDao.findById(userId)).thenReturn(userOptional);
+    	User retrivedUser = userService.findById(userId);
+    	System.out.println(retrivedUser);
+    	assertThat(retrivedUser.getId()).isEqualTo(user.getId());
     }
 }
