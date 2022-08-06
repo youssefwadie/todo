606b1a3fa85d28122db26eae2c724ca8b4845647
Youssef Wadie <youssefwadie14@gmail.com>
2022-08-06 16:11:11 +0200

    font awesome

--- todo-ui/src/app/app.module.ts
+++ todo-ui/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {LoginComponent} from './components/login/login.component';
+import {FormsModule} from '@angular/forms';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {TodoComponent} from './components/todo/todo.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, NgbModule],
+  declarations: [AppComponent, LoginComponent, DashboardComponent, TodoComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, NgbModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
