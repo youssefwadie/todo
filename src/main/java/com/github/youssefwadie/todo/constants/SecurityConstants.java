35de996ae17e7e516d91c00ddb15826a43cb4eba
Youssef Wadie <youssefwadie14@gmail.com>
2022-08-08 12:00:50 +0200

    prevent util, constant classes from being inherited or instantiated

--- src/main/java/com/github/youssefwadie/todo/constants/SecurityConstants.java
+++ src/main/java/com/github/youssefwadie/todo/constants/SecurityConstants.java
@@ -5,13 +5,15 @@ import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 
-public class SecurityConstants {
-	public static final String JWT_KEY = "jxgE#*($#Qe_XH!uPq8Vdby@YFNkANd^u3dQ53YU%n4B";
-	public static final byte[] JWT_TOKEN_BYTES = JWT_KEY.getBytes(StandardCharsets.UTF_8);
-	public static final long JWT_LIFE_TIME = 3000_000; // 5 minutes
+public final class SecurityConstants {
+    private SecurityConstants() {}
 
-	public static final String JWT_AUTHENTICATION_SCHEME = "Bearer ";
-	public static final String JWT_HEADER = "Authorization";
+    public static final String JWT_KEY = "jxgE#*($#Qe_XH!uPq8Vdby@YFNkANd^u3dQ53YU%n4B";
+    public static final byte[] JWT_TOKEN_BYTES = JWT_KEY.getBytes(StandardCharsets.UTF_8);
+    public static final long JWT_LIFE_TIME = 3000_000; // 5 minutes
 
-	public static final SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_TOKEN_BYTES);
+    public static final String JWT_AUTHENTICATION_SCHEME = "Bearer ";
+    public static final String JWT_HEADER = "Authorization";
+
+    public static final SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_TOKEN_BYTES);
 }
